52cf3da3842c20c549e56a51a3f77234582564f7
tampler <socnetfpga@gmail.com>
2021-12-03 00:23:53 +0300

    Initial test impl

--- test/nws_instance_test.go
+++ test/nws_instance_test.go
@@ -1,10 +1,8 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,32 +10,21 @@ import (
 func TestNwsInstanceExample(t *testing.T) {
 	t.Parallel()
 
-	const size = 1
 	cfg := make(map[string]string)
+	name := []string{"VM0"}
 
-	cfg["name"] = fmt.Sprintf("terratest-instance-%s", random.UniqueId())
 	cfg["network_id"] = "b94ccf24-2346-4a9d-9a23-12c46a642e74"
-
-	var ip [size]string
-	var instance_type [size]string
-	var disk_size [size]int
-	var template [size]string
-
-	ip[0] = "10.0.2.16"
-	instance_type[0] = "t2.micro"
-	disk_size[0] = 12
-	template[0] = "Ubuntu 20.04 LTS"
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../examples/basic",
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
-			"name":           cfg["name"],
+			"name":           name,
+			"ip":             []string{"10.0.1.16"},
+			"instance_type":  []string{"t2.micro"},
+			"root_disk_size": []int{12},
+			"template":       []string{"Ubuntu 20.04 LTS"},
 			"network_id":     cfg["network_id"],
-			"ip":             ip,
-			"instance_type":  instance_type,
-			"root_disk_size": disk_size,
-			"template":       template,
 		},
 	})
 
@@ -45,8 +32,9 @@ func TestNwsInstanceExample(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
-	ids := terraform.Output(t, terraformOptions, "id")
+	act_name := terraform.Output(t, terraformOptions, "name")
+	id := terraform.Output(t, terraformOptions, "id")
 
-	// assert.ContainsSameElements(t, 1, ids)
-	assert.Len(t, size, len(ids))
+	assert.Len(t, len(name), len(id))
+	assert.ElementsMatch(t, name, act_name)
 }
