66092caf1d7c0f34e15311eea6e6d4396fa15150
tampler <socnetfpga@gmail.com>
2021-12-03 13:42:33 +0300

    Add test cases

--- test/nws_instance_test.go
+++ test/nws_instance_test.go
@@ -16,6 +16,27 @@ func TestNwsInstanceExample(t *testing.T) {
 
 	exp_name := []string{"VM0", "VM1"}
 
+	testCases := []struct {
+		name          []string
+		ip            []string
+		instance_type []string
+		disk_size     []int
+		template      []string
+		network_id    string
+	}{
+		{
+			[]string{"VM0", "VM1"},
+			[]string{"10.0.1.16", "10.0.1.17"},
+			[]string{"t2.micro", "t2.micro"},
+			[]int{12, 12},
+			[]string{"Ubuntu 20.04 LTS", "Ubuntu 20.04 LTS"},
+			"b94ccf24-2346-4a9d-9a23-12c46a642e74",
+		},
+	}
+	for _, testCase := range testCases {
+
+	}
+
 	stage(t, "deploy", func() {
 		opts := config(t, exp_name, servicePath)
 		test_structure.SaveTerraformOptions(t, servicePath, opts)
